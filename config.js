f96a6ede24b6228561c760fe3b659b8adfdfa76f
Semira-cpcc <67286576+Semira-cpcc@users.noreply.github.com>
2020-07-08 14:12:33 -0400

    Update config.js

--- config.js
+++ config.js
@@ -13,9 +13,36 @@ var marker = L.marker([35.2271, -80.8431], {
 
 marker.bindPopup("Hello from Charlotte, NC!");
 
+var Sam_Marker = L.marker([35.6762, 139.6503], {
+	title: "Sam Bailey wants to travel to Tokyo, Japan"
+});
+
+marker.bindPopup("Sam says Konichiwa from Tokyo!");
+
+var Jonathan_Marker = L.marker([30.0444, 31.2357], {
+	title: "Jonathan  Lindsay want to travel to Egypt});
+
+marker.bindPopup("Jonathan says Hello from Egypt!");
+
+var Semira_Marker = L.marker([7.5400, -5.5471], {
+	title: "Semira Daniels wants to travel to Côte d'Ivoire"
+});
+
+Semira_Marker.bindPopup("Semira says Hello from Côte d'Ivoire!");
+
+var Camryn_Marker = L.marker([13.9094, -60.9789], {
+	title: "Camryn wants to travel to St. Lucia"
+});
+
+Camryn_Marker.bindPopup("Camryn says Hello from St. Lucia!");
+
 var myMap = L.map("map", {
-	center: [35.2271, -80.8431],
-	zoom: 10});
+	center: [0, 0],
+	zoom: 3});
 	
 streets.addTo(myMap);
 marker.addTo(myMap);
+Sam_Marker.addTo(myMap);
+Jonathan_Marker.addTo(myMap);
+Semira_Marker.addTo(myMap);
+Camryn_Marker.addTo(myMap);
